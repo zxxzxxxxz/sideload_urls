bf5d9437a37877220a7cea84f87facc3e84806b7
zxxzxxxxz <zxxzxxxxz@gmail.com>
2025-04-21 02:26:24 +0900

    a

--- app/livecontainer/page.tsx
+++ app/livecontainer/page.tsx
@@ -15,21 +15,26 @@ export default async function () {
     const response = await fetch(releasesUrl);
     const releasesJson = await response.json() as releasesJson;
 
-    const html = releasesJson.map(release => {
-        return <table>
-            <tbody>
-                <tr>
-                    <td style={{padding: '0 4px'}}>{release.tag_name}</td>
-                    <td style={{padding: '0 4px'}}>{release.published_at}</td>
-                    <td style={{padding: '0 4px'}}>
+    return <table>
+        <thead>
+            <tr>
+                <th>Version</th>
+                <th>Date</th>
+                <th>Download</th>
+            </tr>
+        </thead>
+        <tbody>
+            {releasesJson.map(release => {
+                return <tr>
+                    <td>{release.tag_name}</td>
+                    <td>{release.published_at}</td>
+                    <td>
                         {release.assets.map(asset => {
                             return <a href={`livecontainer://install?url=${asset.browser_download_url}`}>{asset.name}</a>
                         })}
                     </td>
-                </tr>
-            </tbody>
-        </table>;
-    });
-
-    return (html);
+                </tr>;
+            })}
+        </tbody>
+    </table>;
 }

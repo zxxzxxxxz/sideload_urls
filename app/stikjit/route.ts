e9925bc5b798cf109a1065d5e6fef2cc6a69dabf
zxxzxxxxz <zxxzxxxxz@gmail.com>
2025-04-22 14:37:40 +0900

    edit description

--- app/stikjit/route.ts
+++ app/stikjit/route.ts
@@ -1,8 +1,18 @@
 import { NextResponse } from "next/server";
 
-const releasesUrl = 'https://api.github.com/repos/StephenDev0/StikJIT/releases';
+const githubUrl = 'https://api.github.com/repos/StephenDev0/StikJIT';
+const githubReleasesUrl = githubUrl + '/releases';
 
-type releasesJson = {
+type githubJson = {
+    name: string,
+    owner: {
+        login: string
+    },
+    html_url: string,
+    description: string
+};
+
+type githubReleasesJson = {
     'tag_name': string,
     'published_at': string,
     'assets': {
@@ -10,12 +20,12 @@ type releasesJson = {
         'size': number,
         'browser_download_url': string
     }[],
-    'body': string;
+    'body': string
 }[];
 
 export async function GET() {
-    const response = await fetch(releasesUrl);
-    const releasesJson = await response.json() as releasesJson;
+    const releaseResponse = await fetch(githubReleasesUrl);
+    const releasesJson = await releaseResponse.json() as githubReleasesJson;
     const releasesJsonFiltered = releasesJson.filter(release => {
         return release.assets.some(asset => asset.browser_download_url.endsWith('.ipa'));
     }).map(release => {
@@ -25,18 +35,24 @@ export async function GET() {
         };
     });
 
+    const githubResponse = await fetch(githubUrl);
+    const githubJson = await githubResponse.json() as githubJson;
+
+    console.log(githubJson);
+
     return NextResponse.json({
-        name: 'StikJIT',
-        subtitle: '',
-        description: '',
+        name: githubJson.name,
+        subtitle: githubJson.description,
+        description: githubJson.description,
         iconURL: 'https://raw.githubusercontent.com/StephenDev0/StikJIT/main/assets/StikJIT_Rounded_Corners.png',
         apps: [
             {
-                name: 'StikJIT',
+                name: githubJson.name,
                 bundleIdentifier: 'com.stik.sj',
-                developerName: 'StephenDev0',
-                localizedDescription: '',
+                developerName: githubJson.owner.login,
+                localizedDescription: githubJson.description,
                 iconURL: 'https://raw.githubusercontent.com/StephenDev0/StikJIT/main/assets/StikJIT_Rounded_Corners.png',
+                website: githubJson.html_url,
                 versions: releasesJsonFiltered.map(release => {
                     return {
                         version: release.tag_name,
